6911020945009219454c348f911c70457f1e3b58
AndreCA1 <carvalhoandre149@gmail.com>
2025-05-13 16:58:16 -0300

    Criação do projeto

---
+++ HOTELBAO/src/test/java/ifmg/edu/br/HOTELBAO/HotelbaoApplicationTests.java
@@ -0,0 +1,13 @@
+package ifmg.edu.br.HOTELBAO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelbaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
