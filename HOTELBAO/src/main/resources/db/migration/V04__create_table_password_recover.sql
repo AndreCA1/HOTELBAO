6d21b986e2f9c5328fd9a1a9ad9e7aca3ea3e127
Ivan Marcelino <ivanmarcelino2004@gmail.com>
2025-07-04 12:38:22 -0300

    Modificando migration para generic

--- HOTELBAO/src/main/resources/db/migration/V04__create_table_password_recover.sql
+++ HOTELBAO/src/main/resources/db/migration/V04__create_table_password_recover.sql
@@ -3,4 +3,4 @@ CREATE TABLE `password_recover` (`id` BIGINT NOT NULL AUTO_INCREMENT,
                                  `email` varchar(150) NOT NULL,
                                  `expiration` datetime NOT NULL,
                                  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+)
